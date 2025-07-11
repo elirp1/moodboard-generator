886820c5573f16b703e5cd0c08d1e59037460836
Eli Prushansky <153029466+elirp1@users.noreply.github.com>
2025-07-11 14:52:59 -0400

    Update pdf_export.py

--- utils/pdf_export.py
+++ utils/pdf_export.py
@@ -1,4 +1,4 @@
-def create_pdf(moodboard_path, palettes, filename="moodboard.pdf"):
+def create_pdf(moodboard_path, moodboard_size, palettes, filename="moodboard.pdf"):
     from reportlab.lib.pagesizes import letter
     from reportlab.pdfgen import canvas
 
@@ -6,7 +6,22 @@ def create_pdf(moodboard_path, palettes, filename="moodboard.pdf"):
     c = canvas.Canvas(filename, pagesize=letter)
 
     def add_moodboard():
-        c.drawImage(moodboard_path, 50, page_height - 350, width=500, height=300)
+        moodboard_width, moodboard_height = moodboard_size
+        aspect_ratio = moodboard_width / moodboard_height
+        max_width = page_width - 100
+        max_height = 300
+
+        if moodboard_width > moodboard_height:
+            draw_width = max_width
+            draw_height = max_width / aspect_ratio
+        else:
+            draw_height = max_height
+            draw_width = max_height * aspect_ratio
+
+        x = (page_width - draw_width) / 2
+        y = page_height - draw_height - 50
+
+        c.drawImage(moodboard_path, x, y, width=draw_width, height=draw_height)
 
     add_moodboard()
 
@@ -17,7 +32,7 @@ def create_pdf(moodboard_path, palettes, filename="moodboard.pdf"):
 
     for palette in palettes:
         for color in palette:
-            if y < 50:  # Start a new page if space runs out
+            if y < 50:
                 c.showPage()
                 add_moodboard()
                 y = page_height - 400
